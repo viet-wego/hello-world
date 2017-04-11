5ad53035370a98f5a74321d6d7433e9c7caa1fac
Viet Tran <tdviet12@gmail.com>
2017-04-11 17:53:30 +0700

    add examples

---
+++ examples/Values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello " + "Viet.")
+
+	fmt.Println("7 + 7 = ", 7 + 7)
+	fmt.Println("9.5 / 3.0 = ", 9.5 / 3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
